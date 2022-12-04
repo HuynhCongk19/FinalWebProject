32b6f8c598777e519b299316c04973bcd640aa1f
HuynhCongk19 <94981627+HuynhCongk19@users.noreply.github.com>
2022-12-04 10:19:09 +0700

    make it better

--- src/main/java/com/ute/FinalProject/utils/DbUtils.java
+++ src/main/java/com/ute/FinalProject/utils/DbUtils.java
@@ -4,7 +4,7 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 public class DbUtils {
-    static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/huynh", "root" , "");
+    static Sql2o sql2o = new Sql2o("jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_a376b9b87e8aecd", "baea2f57ceeb07" , "5fe7c274");
 
     public static Connection getConnection(){
         return sql2o.open();
