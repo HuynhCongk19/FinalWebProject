00bf62bddf994346f015331ff6a3edfc0d6fce08
HuynhCongk19 <94981627+HuynhCongk19@users.noreply.github.com>
2022-12-06 20:33:43 +0700

    test deploy với git

--- src/main/java/com/ute/FinalProject/utils/DbUtils.java
+++ src/main/java/com/ute/FinalProject/utils/DbUtils.java
@@ -5,7 +5,6 @@ import org.sql2o.Sql2o;
 
 public class DbUtils {
     static Sql2o sql2o = new Sql2o("jdbc:mysql://us-cdbr-east-06.cleardb.net:3306/heroku_29b86e1e5c4bc7b", "b982cea641f865" , "8185de48");
-    //database kết nối online
     //static Sql2o sql2o = new Sql2o("jdbc:mysql://localhost:3306/huynh", "root" , "");
     public static Connection getConnection(){
         return sql2o.open();
